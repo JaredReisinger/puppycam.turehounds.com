7e7bce436edc19fbd8a23e01077c14ee6ee535d2
Jared Reisinger <jaredreisinger@hotmail.com>
2021-01-09 01:08:02 -0700

    actual sensor calls

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
-	// import tureHoundsLogo from "images/ture-hounds-logo.png";
+	import { onMount } from "svelte";
+	import { DateTime } from "luxon";
+
 	// let dummy = true;
 	let dummy = false;
+
+	// try fetching from puppycam-sensor!
+	const sensorApi = "https://puppycam-sensor.spudnoggin.com/sensor";
+
+	let observed: DateTime;
+	let temperature: number;
+	let humidity: number;
+
+	const dateFmt = { ...DateTime.DATETIME_SHORT, timeZoneName: "short" };
+
+	async function updateWeather() {
+		const sample = await (await fetch(sensorApi)).json();
+
+		observed = DateTime.fromISO(sample.observed).toLocaleString(dateFmt);
+		temperature = sample.temperature;
+		humidity = sample.humidity;
+	}
+
+	onMount(() => {
+		updateWeather();
+		const interval = setInterval(updateWeather, 30 * 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <style>
@@ -42,6 +69,65 @@
 	.details {
 		padding: 0.5em 1em;
 		flex: auto;
+	}
+
+	.details > *:first-child {
+		margin-top: 0;
+	}
+
+	th {
+		text-align: left;
+	}
+
+	.blue {
+		color: hsl(220, 80%, 50%);
+	}
+	.red {
+		color: hsl(0, 80%, 50%);
+	}
+	.yellow {
+		color: hsl(60, 80%, 45%);
+	}
+	.white {
+		color: hsl(0, 0%, 50%);
+	}
+	.green {
+		color: hsl(100, 80%, 30%);
+	}
+	.brown {
+		color: hsl(50, 80%, 30%);
+	}
+
+	.weather {
+		max-width: 14em;
+		display: flex;
+		justify-content: space-evenly;
+		margin: 1em 0;
+		font-size: 120%;
+		font-weight: bold;
+	}
+
+	/* .temperature,
+	.humidity {
+		font-weight: bold;
+	} */
+
+	.temperature::after {
+		content: "°F";
+	}
+
+	.humidity::after {
+		content: "% RH";
+	}
+
+	.observed {
+		font-size: 80%;
+		font-style: italic;
+		color: hsl(0, 0%, 50%);
+	}
+
+	.observed::before {
+		content: "as of ";
 	}
 
 	@media (max-width: 1024px) {
@@ -103,15 +189,56 @@
 	</div>
 
 	<div class="details">
-		<h1>Details...</h1>
-		<p>Here’s where some details would go…</p>
-		<p>Blue, Red, Yellow, White, Green...</p>
-		<p>
-			Current puppy weather:<br />
-			&nbsp;&nbsp;&nbsp;temperature:
-			<b>XX&deg;F</b><br />
-			&nbsp;&nbsp;&nbsp;humidity:
-			<b>XX%</b>
-		</p>
+		<h3>The puppies</h3>
+		<table>
+			<thead>
+				<tr>
+					<th>color</th>
+					<th>sex</th>
+					<th>weight</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="blue">
+					<td>Blue</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+				<tr class="red">
+					<td>Red</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+				<tr class="yellow">
+					<td>Yellow</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+				<tr class="white">
+					<td>White</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+				<tr class="green">
+					<td>Green</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+				<tr class="brown">
+					<td>Brown</td>
+					<td>???</td>
+					<td>???</td>
+				</tr>
+			</tbody>
+		</table>
+
+		{#if observed}
+			<h3>Current puppy weather</h3>
+			<div class="weather">
+				<div class="temperature">{temperature}</div>
+				<div class="humidity">{humidity}</div>
+			</div>
+			<div class="observed">{observed}</div>
+		{/if}
 	</div>
 </div>
