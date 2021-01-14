cf3564e8034e192b4571cecd3d4f2689646542eb
Jared Reisinger <jaredreisinger@hotmail.com>
2021-01-13 21:22:39 -0700

    add puppy details

--- src/components/PuppyDetails.svelte
+++ src/components/PuppyDetails.svelte
@@ -2,8 +2,27 @@
 </script>
 
 <style>
-    th {
+    tr > * { 
         text-align: left;
+    }
+
+    tr > :nth-child(n+2) {
+        padding-left: 0.5em;
+    }
+
+    tr > :nth-child(2) {
+        text-align: center;
+    }
+
+    tr > :nth-child(4) {
+        text-align: center;
+    }
+
+    .note {
+        margin-top: 0.5em;
+        font-size: 80%;
+        font-style: italic;
+        color: hsl(0, 0%, 50%);
     }
 
     .blue {
@@ -26,51 +45,54 @@
     }
 </style>
 
+<p>All puppies were born on Wednesday, January 13, 2021.</p>
+
 <table>
     <thead>
         <tr>
             <th>collar</th>
             <th>sex</th>
             <th>color</th>
-            <th>weight</th>
+            <th>weight (oz)</th>
         </tr>
     </thead>
     <tbody>
         <tr class="blue">
             <td>Blue</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>F</td>
+            <td>Red &amp; White</td>
+            <td>8.2</td>
         </tr>
         <tr class="red">
             <td>Red</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>M</td>
+            <td>Red &amp; White</td>
+            <td>7.2</td>
         </tr>
         <tr class="yellow">
             <td>Yellow</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>F</td>
+            <td>Red &amp; White</td>
+            <td>7.2</td>
         </tr>
         <tr class="white">
             <td>White</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>F</td>
+            <td>Red &amp; White</td>
+            <td>7.9</td>
         </tr>
         <tr class="green">
             <td>Green</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>M</td>
+            <td>Red &amp; White</td>
+            <td>8.1</td>
         </tr>
         <tr class="pink-purple">
             <td>Pink/Purple</td>
-            <td>???</td>
-            <td>???</td>
-            <td>???</td>
+            <td>F</td>
+            <td>Tri-colored</td>
+            <td>7.6</td>
         </tr>
     </tbody>
 </table>
+<p class="note">Weight listed is weight at birth.</p>
