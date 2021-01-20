2c48ca5b73f3532f385f90e387b05db94f192dd9
Jared Reisinger <jaredreisinger@hotmail.com>
2021-01-20 10:28:58 -0700

    fixed typings on Axis

--- src/components/Axis.svelte
+++ src/components/Axis.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
   import { select /* , selectAll */ } from "d3-selection";
   import { axisBottom, axisLeft } from "d3-axis";
-  import type { Scale } from "d3-scale";
+  import type { Axis, AxisDomain, AxisScale } from "d3-axis";
 
-  export let width: number = undefined;
+  // export let width: number = undefined;
   export let height: number = undefined;
   export let margin: number = undefined;
   export let position: string = undefined;
-  export let scale: Scale = undefined;
+  export let scale: AxisScale<AxisDomain> = undefined;
 
   let transform;
   let g;
 
   $: {
     select(g).selectAll("*").remove();
-    let axis;
+
+    let axis: Axis<AxisDomain>;
     switch (position) {
       case "bottom":
         axis = axisBottom(scale).tickSizeOuter(0);
@@ -24,6 +25,7 @@
         axis = axisLeft(scale).tickSizeOuter(0);
         transform = `translate(${margin}, 0)`;
     }
+
     select(g).call(axis).style("font-family", "inherit");
   }
 </script>
