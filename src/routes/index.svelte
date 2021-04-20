d5f131dc9da3e02d7f94ad2107f18681a2d42aa5
Jared Reisinger <jaredreisinger@hotmail.com>
2021-04-20 16:58:01 -0700

    back to single video feed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,14 +15,6 @@
     <div class="aspect-main">
       <!-- <Video title="Eye in the Sky" videoId="jqbAk2MNMd" /> -->
       <Video2
-        publicId="jqbAk2MNMd"
-        uuid="9c06bf22fa9047559a39ced47953a9ec"
-        streamHost="stream-ue1-bravo.dropcam.com"
-      />
-    </div>
-    <div class="aspect-child">
-      <!-- <Video title="Up-close" videoId="dAYyHiFXJU" /> -->
-      <Video2
         publicId="dAYyHiFXJU"
         uuid="3ed3fea78dfc4f86968ab2ae9f616443"
         streamHost="stream-ue1-charlie.dropcam.com"
@@ -52,11 +44,10 @@
     /* height: auto; */
   }
 
-  .aspect-main,
-  .aspect-child {
+  .aspect-main /* ,
+  .aspect-child */ {
     /* ensures a 16:9 aspect ratio */
     width: min(100vw - 1rem, (100vh - 5rem) * 1.77778);
-    /* height: min(100vh - 5rem, (100vw - 1rem) / 1.77778); */
     margin-bottom: 0.5rem;
     background-color: hsl(0, 0%, 40%);
   }
@@ -78,25 +69,22 @@
     .aspect-root {
       /* ensures a 16:9 aspect ratio */
       width: min(100vw - 25rem, (100vh - 10rem) * 1.77778);
-      /* height: min(100vh - 10rem, (100vw - 25rem) / 1.77778); */
       position: relative;
       flex-basis: content;
     }
 
     .aspect-main {
       width: 100%;
-      /* height: 100%; */
     }
 
-    .aspect-child {
+    /* .aspect-child {
       position: absolute;
       width: 30%;
-      /* height: 30%; */
       top: 0;
       right: 0;
       margin: 0.5em;
       border: 0.25em solid white;
-    }
+    } */
 
     .details {
       padding: 0.5em 1em;
