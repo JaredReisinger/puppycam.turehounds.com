1d15639f351c0382f2f98ab3774ddd4dc9f78c1d
Jared Reisinger <jaredreisinger@hotmail.com>
2024-12-17 17:56:21 -0700

    weights, stream

--- src/lib/video-streams.ts
+++ src/lib/video-streams.ts
@@ -1,5 +1,5 @@
 // YoutTub stream (e.g. https://www.youtube.com/live/FoBs4KvnR8M)
-export const puppycam1_youtube = 'XvwlME5esZI';
+export const puppycam1_youtube = 'iKM2W40iSVQ';
 
 // old Nest streams..
 export const nestcamOffice = 'VuqBb1RtMN';
